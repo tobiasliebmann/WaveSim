959ee80343778be7e78d8f6eff9e15ced8bdef50
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-25 19:45:53 +0100

    Made the code look nicer.

--- simulation_playground.py
+++ simulation_playground.py
@@ -9,7 +9,7 @@ a0 = np.array([0, -1, 1, -1, 1, -1, 1, -1, 1, 0])
 # Initial velocities of the amplitudes.
 v0 = np.array([0, 0, 1, 1, -1, 0, 0, 0, 0, 0])
 # Grid spacing.
-dx: float = 0.1
+dx = 0.1
 # Spacing of the time steps.
 dt = 0.005
 # speed of sound.
@@ -22,8 +22,10 @@ my_sim = sim1D.Numeric1DWaveSimulator(dx, dt, c, n, t, a0, v0)
 result = my_sim.run()
 print(result)
 
+# Here begins the visualization part.
+
 fig = plt.figure()
-ax = plt.axes(xlim=(0, 1), ylim=(-2, 2))
+ax = plt.axes(xlim=(0, 1), ylim=(-1.5, 1.5))
 line, = ax.plot([], [], lw=2)
 
 
@@ -35,14 +37,13 @@ def init():
 
 # animation function.  This is called sequentially
 def animate(i):
-    x = np.linspace(0, 1, len(result[i]))
+    x = np.linspace(0, dx * n, n)
     y = result[i]
     line.set_data(x, y)
     return line,
 
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=len(result[1]), interval=500, blit=True)
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=t, interval=500, blit=True, repeat=True)
 
 plt.show()
