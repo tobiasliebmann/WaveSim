32132a1f462fea51f7fe13f08328d27cd961fb0b
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-25 18:13:34 +0100

    The basic simulation works now.

--- simulation_playground.py
+++ simulation_playground.py
@@ -1,7 +1,5 @@
 import simulator1D as sim1D
 
-import visualization as vis
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -23,23 +21,28 @@ t = 11
 my_sim = sim1D.Numeric1DWaveSimulator(dx, dt, c, n, t, a0, v0)
 result = my_sim.run()
 print(result)
-fig, ax = plt.subplots()
 
-t = 
-s = np.sin(t)
-l = plt.plot(t, s)
-
-ax = plt.axis([0, TWOPI, -1, 1])
-
-redDot, = plt.plot([], [], "ro")
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 1), ylim=(-2, 2))
+line, = ax.plot([], [], lw=2)
 
 
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+
+# animation function.  This is called sequentially
 def animate(i):
-    redDot.set_data(np.array([i, i + 1]), np.array([np.sin(i), np.sin(i + 1)]))
-    return redDot,
+    x = np.linspace(0, 1, len(result[i]))
+    y = result[i]
+    line.set_data(x, y)
+    return line,
 
 
-# create animation using the animate() function
-my_animation = animation.FuncAnimation(fig, animate, frames=np.arange(0.0, TWOPI, 0.1), interval=30, blit=True,
-                                       repeat=True)
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=len(result[1]), interval=500, blit=True)
+
 plt.show()
