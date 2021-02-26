be1f874a36b3e3ac12ad1b0beca5ac345c00ff29
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-26 21:16:53 +0100

    The import is now working and I wrote the first test.

--- tests/simulator1D_test.py
+++ tests/simulator1D_test.py
@@ -9,24 +9,25 @@ class Sim1DTest(ut.TestCase):
 
     initial_positions = np.array([0, 0, 0, 0, 0, 0])
 
-    initial_velocities = np.array([1, 1, 1, 1, 1, 1])
+    initial_velocities = np.array([0, 1, 1, 1, 1, 0])
 
     # Initialize simulator
     my_sim = sim.Numeric1DWaveSimulator(1, 1, 0.5, 6, 6, initial_positions, initial_velocities)
 
     def test_create_time_step_matrix(self):
-        compare_matrix = np.ndarray([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0. ],
-                                         [0.7, 0.6, 0.7, 0., 0., 0., 0., 0., 0., 0.],
+        compare_matrix = np.array([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                                         [0., 0.6, 0.7, 0., 0., 0., 0., 0., 0., 0.],
                                          [0., 0.7, 0.6, 0.7, 0., 0., 0., 0., 0., 0.],
                                          [0., 0., 0.7, 0.6, 0.7, 0., 0., 0., 0., 0.],
                                          [0., 0., 0., 0.7, 0.6, 0.7, 0., 0.,  0., 0.],
                                          [0., 0., 0., 0., 0.7, 0.6, 0.7, 0., 0., 0.],
                                          [0., 0., 0., 0., 0., 0.7, 0.6, 0.7, 0., 0.],
                                          [0., 0., 0., 0., 0., 0., 0.7, 0.6, 0.7, 0.],
-                                         [0., 0., 0., 0., 0., 0., 0., 0.7, 0.6, 0.7],
+                                         [0., 0., 0., 0., 0., 0., 0., 0.7, 0.6, 0.],
                                          [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]])
-
-        self.assertEqual(self.my_sim.create_time_step_matrix(10, 0.7), compare_matrix)
+        constructed_matrix = self.my_sim.create_time_step_matrix(10, 0.7)
+        print(constructed_matrix)
+        np.testing.assert_almost_equal(constructed_matrix, compare_matrix)
 
 
 if __name__ == "__main__":
