06435b8613a8d04e40009ac395bcd90ef4da1728
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-03-01 17:30:49 +0100

    I will use numpy to save and load my data.

--- pandas_test.py
+++ pandas_test.py
@@ -4,7 +4,6 @@ import numpy as np
 
 import datetime as dt
 
-
 # df = pd.read_csv("test_data2.csv")
 # new_row = pd.DataFrame({"First": "Test 1", "Second": "Test 2", "Third": [np.array([[1., 2.], [3., 4.], [5., 6.]]).tolist()]})
 # with open("test_data2.csv", "a") as file:
@@ -12,6 +11,23 @@ import datetime as dt
 
 print(isinstance(dt.datetime.utcnow(), dt.datetime))
 
+num = 5.
+my_array = np.array([1., 2., 3.])
+my_matrix = np.array([[1., 2., 3.], [4., 5., 6.]])
+
+"""
+df = pd.DataFrame({"First": num,
+                   "Second": [pd.array(my_array)],
+                   "Third": [pd.DataFrame(my_matrix)]})
+
+with open("new_test_data.csv", "a") as file:
+    df.to_csv(file, header=True, index=False, sep=";")
+"""
+
+df2 = pd.read_csv("new_test_data.csv", delimiter=";")
+print(pd.array(df2["Second"]))
+
+"""
 # Try to load data from a file
 print("Start loading data.")
 df = pd.read_csv("test_data/wave_sim1D_2021-03-01 15:05:26.csv", delimiter=";", dtype={
@@ -28,4 +44,4 @@ with np.printoptions(threshold=np.inf):
     new_array = df["result"].to_numpy()
     print(type(new_array))
     # print(new_array)
-
+"""
