38131b88a5f5832dc2d0d7a4b4b07f260af34f78
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-03-19 19:25:56 +0100

    I managed to speed up the code in the little experiments I did. I will to transfer this knowledge to the actual simulation.

--- speed_up_experiments/experiments.py
+++ speed_up_experiments/experiments.py
@@ -1,9 +1,8 @@
 import numpy as np
 import random as rd
-import timeit as ti
+# import timeit as ti
 import time
 import numba as nb
-import scipy
 
 dim = 500
 
@@ -26,40 +25,122 @@ def create_matrix():
 multiply_matrix = create_matrix()
 state_matrix = np.random.rand(dim, dim)
 
-print(multiply_matrix.dtype)
-print(state_matrix.dtype)
+
+def mat_mul(matrix1, matrix2):
+    """
+
+    :param matrix1:
+    :param matrix2:
+    :return:
+    """
+    return np.dot(matrix1, matrix2) + np.dot(matrix1, matrix2)
 
 
-def mat_mul():
-    return np.dot(multiply_matrix, state_matrix) + np.dot(state_matrix, multiply_matrix)
+def opt_mat_mul(matrix):
+    """
 
-
-def opt_mat_mul():
-    temp1 = np.roll(state_matrix, dim)
-    temp2 = np.roll(state_matrix, -dim)
-    temp3 = np.roll(state_matrix.T, dim)
-    temp4 = np.roll(state_matrix.T, -dim)
+    :param matrix:
+    :return:
+    """
+    temp1 = np.roll(matrix, dim)
+    temp2 = np.roll(matrix, -dim)
+    temp3 = np.roll(matrix.T, dim)
+    temp4 = np.roll(matrix.T, -dim)
     zeros = np.zeros(dim)
     temp1[0] = zeros
     temp2[dim - 1] = zeros
     temp3[0] = zeros
     temp4[dim - 1] = zeros
-    return (2. - 4. * num) * state_matrix + num * (temp1 + temp2 + temp3.T + temp4.T)
+    return (2. - 4. * num) * matrix + num * (temp1 + temp2 + temp3.T + temp4.T)
 
 
 # print(opt_mat_mul() - mat_mul())
 
-mat_mul()
 
-start2 = time.time()
-[opt_mat_mul() for _ in range(times_called)]
-end2 = time.time()
+def run_mat_mul(times):
+    """
+
+    :param times:
+    :return:
+    """
+    for _ in range(times):
+        mat_mul(multiply_matrix, state_matrix)
+
+
+def run_opt_mat_mul(times):
+    """
+
+    :param times:
+    :return:
+    """
+    for _ in range(times):
+        opt_mat_mul(state_matrix)
+
+
+@nb.jit(nopython=True)
+def run_jited_mat_mul(matrix1, matrix2, times):
+    """
+
+    :param matrix1:
+    :param matrix2:
+    :param times:
+    :return:
+    """
+    for _ in range(times):
+        return np.dot(matrix1, matrix2) + np.dot(matrix1, matrix2)
+
+
+@nb.jit(nopython=True)
+def run_jited_opt_mat_mul(matrix, times):
+    """
+
+    :param matrix:
+    :param times:
+    :return:
+    """
+    for _ in range(times):
+        temp1 = np.roll(matrix, dim)
+        temp2 = np.roll(matrix, -dim)
+        temp3 = np.roll(matrix.T, dim)
+        temp4 = np.roll(matrix.T, -dim)
+        zeros = np.zeros(dim)
+        temp1[0] = zeros
+        temp2[dim - 1] = zeros
+        temp3[0] = zeros
+        temp4[dim - 1] = zeros
+        return (2. - 4. * num) * matrix + num * (temp1 + temp2 + temp3.T + temp4.T)
+
 
 start1 = time.time()
-[mat_mul() for _ in range(times_called)]
+run_jited_opt_mat_mul(state_matrix, times_called)
 end1 = time.time()
+
+start2 = time.time()
+run_jited_mat_mul(multiply_matrix, state_matrix, times_called)
+end2 = time.time()
+
+start3 = time.time()
+run_jited_mat_mul(multiply_matrix, state_matrix, times_called)
+end3 = time.time()
+
+start4 = time.time()
+run_jited_opt_mat_mul(state_matrix, times_called)
+end4 = time.time()
+
+start5 = time.time()
+run_mat_mul(times_called)
+end5 = time.time()
+
+start6 = time.time()
+run_opt_mat_mul(times_called)
+end6 = time.time()
 
 # print(f"Function call needed {ti.timeit(mat_mul)} ms.")
 # print(f"Optimized function call needed {ti.timeit(opt_mat_mul)} ms.")
-print((end1 - start1))
-print((end2 - start2))
+print(f"Parameters: matrix dimension = {dim}x{dim}, function calls = {times_called}.")
+print(f"For the first time the jited standard multiplication takes {np.round(end1 - start1, 4)} s.")
+print(f"For the first time the jited optimized multiplication takes {np.round(end2 - start2, 4)} s.")
+print(f"For the second time the jited standard multiplication takes {np.round(end3 - start3, 4)} s.")
+print(f"For the second time the jited optimized multiplication takes {np.round(end4 - start4, 4)} s.")
+print(f"The standard multiplication takes {np.round(end5 - start5, 2)} s.")
+print(f"The optimized multiplication takes {np.round(end6 - start6, 2)} s.")
