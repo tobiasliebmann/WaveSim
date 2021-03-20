2ba9795a00de4937e444f7c96011ec53d330c876
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-03-20 18:11:16 +0100

    Made a first little benchmark plot.

--- speed_up_experiments/experiments.py
+++ speed_up_experiments/experiments.py
@@ -1,29 +1,39 @@
 import numpy as np
 import random as rd
-# import timeit as ti
+import matplotlib.pyplot as plt
 import time
 import numba as nb
 
-dim = 500
+dimensions = np.array([10, 50, 100, 200, 300, 400, 500])
+dim = 100
 
-times_called = 300
+times_called = np.array([10, 50, 100, 200, 300, 400, 500])
+calls = 100
 
 num = rd.random()
 
+# Arrays saving the data for runs differing how often a function is called but with a fixed dimension.
+normal_time_data = np.array([])
+opt_time_data = np.array([])
+jited_normal_time_data = np.array([])
+jited_opt_time_data = np.array([])
 
-def create_matrix():
+# Arrays saving the data for runs differing in dimension but with a fixing the times a function is called.
+normal_dim_data = np.array([])
+opt_dim_data = np.array([])
+jited_normal_dim_data = np.array([])
+jited_opt_dim_data = np.array([])
+
+
+def create_matrix(dimension):
     """
 
     :return:
     """
-    matrix = (1 - 2 * num) * np.identity(dim)
+    matrix = (1 - 2 * num) * np.identity(dimension)
     np.fill_diagonal(matrix[1:], num)
     np.fill_diagonal(matrix[:, 1:], num)
     return matrix
-
-
-multiply_matrix = create_matrix()
-state_matrix = np.random.rand(dim, dim)
 
 
 def mat_mul(matrix1, matrix2):
@@ -57,48 +67,48 @@ def opt_mat_mul(matrix):
 # print(opt_mat_mul() - mat_mul())
 
 
-def run_mat_mul(times):
+def run_mat_mul(number_of_calls):
     """
 
-    :param times:
+    :param number_of_calls:
     :return:
     """
-    for _ in range(times):
+    for _ in range(number_of_calls):
         mat_mul(multiply_matrix, state_matrix)
 
 
-def run_opt_mat_mul(times):
+def run_opt_mat_mul(number_of_calls):
     """
 
-    :param times:
+    :param number_of_calls:
     :return:
     """
-    for _ in range(times):
+    for _ in range(number_of_calls):
         opt_mat_mul(state_matrix)
 
 
 @nb.jit(nopython=True)
-def run_jited_mat_mul(matrix1, matrix2, times):
+def run_jited_mat_mul(matrix1, matrix2, number_of_calls):
     """
 
     :param matrix1:
     :param matrix2:
-    :param times:
+    :param number_of_calls:
     :return:
     """
-    for _ in range(times):
+    for _ in range(number_of_calls):
         return np.dot(matrix1, matrix2) + np.dot(matrix1, matrix2)
 
 
 @nb.jit(nopython=True)
-def run_jited_opt_mat_mul(matrix, times):
+def run_jited_opt_mat_mul(matrix, number_of_calls):
     """
 
     :param matrix:
-    :param times:
+    :param number_of_calls:
     :return:
     """
-    for _ in range(times):
+    for _ in range(number_of_calls):
         temp1 = np.roll(matrix, dim)
         temp2 = np.roll(matrix, -dim)
         temp3 = np.roll(matrix.T, dim)
@@ -111,36 +121,81 @@ def run_jited_opt_mat_mul(matrix, times):
         return (2. - 4. * num) * matrix + num * (temp1 + temp2 + temp3.T + temp4.T)
 
 
-start1 = time.time()
-run_jited_opt_mat_mul(state_matrix, times_called)
-end1 = time.time()
+multiply_matrix = create_matrix(dim)
+state_matrix = np.random.rand(dim, dim)
 
-start2 = time.time()
-run_jited_mat_mul(multiply_matrix, state_matrix, times_called)
-end2 = time.time()
+for times in times_called:
+    start1 = time.time()
+    run_jited_opt_mat_mul(state_matrix, times)
+    end1 = time.time()
+    jited_opt_time_data = np.append(jited_opt_time_data, end1 - start1)
 
-start3 = time.time()
-run_jited_mat_mul(multiply_matrix, state_matrix, times_called)
-end3 = time.time()
+    start2 = time.time()
+    run_jited_mat_mul(multiply_matrix, state_matrix, times)
+    end2 = time.time()
+    jited_normal_time_data = np.append(jited_normal_time_data, end2 - start2)
 
-start4 = time.time()
-run_jited_opt_mat_mul(state_matrix, times_called)
-end4 = time.time()
+    start3 = time.time()
+    run_mat_mul(times)
+    end3 = time.time()
+    normal_time_data = np.append(normal_time_data, end3 - start3)
 
-start5 = time.time()
-run_mat_mul(times_called)
-end5 = time.time()
+    start4 = time.time()
+    run_opt_mat_mul(times)
+    end4 = time.time()
+    opt_time_data = np.append(opt_time_data, end4 - start4)
 
-start6 = time.time()
-run_opt_mat_mul(times_called)
-end6 = time.time()
+for dimension in dimensions:
+
+    multiply_matrix = create_matrix(dimension)
+    state_matrix = np.random.rand(dimension, dimension)
+
+    start1 = time.time()
+    run_jited_opt_mat_mul(state_matrix, calls)
+    end1 = time.time()
+    jited_opt_dim_data = np.append(jited_opt_dim_data, end1 - start1)
+
+    start2 = time.time()
+    run_jited_mat_mul(multiply_matrix, state_matrix, calls)
+    end2 = time.time()
+    jited_normal_dim_data = np.append(jited_normal_dim_data, end2 - start2)
+
+    start3 = time.time()
+    run_mat_mul(calls)
+    end3 = time.time()
+    normal_dim_data = np.append(normal_dim_data, end3 - start3)
+
+    start4 = time.time()
+    run_opt_mat_mul(calls)
+    end4 = time.time()
+    opt_dim_data = np.append(opt_dim_data, end4 - start4)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.plot(times_called, normal_time_data, marker="s")
+ax.plot(times_called, opt_time_data, marker="o")
+ax.plot(times_called, jited_normal_time_data, marker="d")
+ax.plot(times_called, jited_opt_time_data, marker="*")
+
+ax.set_ylabel("execution time (s)")
+ax.set_xlabel("times called")
+ax.set_yscale("log")
+ax.set_title(f"Matrix multiplication execution times for matrix dimension {dim}x{dim}.")
+ax.legend(["Normal", "Optimized", "Jited and normal", "Jited and optimized"], loc="upper left", bbox_to_anchor=(0.075, .995))
+
+plt.grid(True)
+plt.draw()
+plt.show()
 
 # print(f"Function call needed {ti.timeit(mat_mul)} ms.")
 # print(f"Optimized function call needed {ti.timeit(opt_mat_mul)} ms.")
-print(f"Parameters: matrix dimension = {dim}x{dim}, function calls = {times_called}.")
-print(f"For the first time the jited standard multiplication takes {np.round(end1 - start1, 4)} s.")
-print(f"For the first time the jited optimized multiplication takes {np.round(end2 - start2, 4)} s.")
-print(f"For the second time the jited standard multiplication takes {np.round(end3 - start3, 4)} s.")
-print(f"For the second time the jited optimized multiplication takes {np.round(end4 - start4, 4)} s.")
-print(f"The standard multiplication takes {np.round(end5 - start5, 2)} s.")
-print(f"The optimized multiplication takes {np.round(end6 - start6, 2)} s.")
+# print(f"Parameters: matrix dimension = {dim}x{dim}, function calls = {calls}.")
+# print(f"For the first time the jited standard multiplication takes {np.round(end1 - start1, 4)} s.")
+# print(f"For the first time the jited optimized multiplication takes {np.round(end2 - start2, 4)} s.")
+# print(f"For the second time the jited standard multiplication takes {np.round(end3 - start3, 4)} s.")
+# print(f"For the second time the jited optimized multiplication takes {np.round(end4 - start4, 4)} s.")
+# print(f"The standard multiplication takes {np.round(end5 - start5, 2)} s.")
+# print(f"The optimized multiplication takes {np.round(end6 - start6, 2)} s.")
+
+
