82c6a3e39e4b535bde9d2147234112c4694ba094
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-03-21 16:04:19 +0100

    Added documentation for the benchmarks.

--- speed_up_experiments/experiments.py
+++ speed_up_experiments/experiments.py
@@ -5,31 +5,14 @@ import time
 import numba as nb
 
 
-dimensions = np.array([10, 50, 100, 200, 300, 400, 500])
-dim = 100
-
-times_called = np.array([10, 50, 100, 200, 300, 400, 500])
-calls = 100
-
-num = rd.random()
-
-# Arrays saving the data for runs differing how often a function is called but with a fixed dimension.
-normal_time_data = np.array([])
-opt_time_data = np.array([])
-jited_normal_time_data = np.array([])
-jited_opt_time_data = np.array([])
-
-# Arrays saving the data for runs differing in dimension but with a fixing the times a function is called.
-normal_dim_data = np.array([])
-opt_dim_data = np.array([])
-jited_normal_dim_data = np.array([])
-jited_opt_dim_data = np.array([])
-
-
-def create_matrix(number, matrix_dimension):
+def create_matrix(number: float, matrix_dimension: int) -> np.ndarray:
     """
-
-    :return:
+    This function creates a quadratic matrix with a populated diagonal, populated off-diagonals and dimension
+    matrix_dimension. All entries in the diagonal are (1 - 2 * number) while the entreis in the off-diagonal are the
+    number number.
+    :param number: A float number which populates the off-diagonals of the matrix.
+    :param matrix_dimension: Dimension of the quadratic matrix which is returned.
+    :return: The matrix constructed as described above.
     """
     matrix = (1 - 2 * number) * np.identity(matrix_dimension)
     np.fill_diagonal(matrix[1:], number)
@@ -37,86 +20,78 @@ def create_matrix(number, matrix_dimension):
     return matrix
 
 
-def mat_mul(matrix1, matrix2):
+def mat_mul(matrix1: np.ndarray, matrix2: np.ndarray) -> np.ndarray:
     """
-
-    :param matrix1:
-    :param matrix2:
-    :return:
+    Calculates the anti-commutator of two matrices.
+    :param matrix1: A matrix.
+    :param matrix2: Another matrix.
+    :return: The Anti-commutator matrix1 and matrix2.
     """
     return np.dot(matrix1, matrix2) + np.dot(matrix1, matrix2)
 
 
-def opt_mat_mul(number, mat_dim, matrix):
+def opt_mat_mul(number: float, matrix_dimension: int, matrix: np.ndarray) -> np.ndarray:
     """
-
-    :param number:
-    :param mat_dim:
-    :param matrix:
-    :return:
+    This function is supposed to be an optimized version of the anti commutator with a matrix that is created by the
+    create_matrix function.
+    :param number: Float number for the created matrix.
+    :param matrix_dimension: Dimension of the created matrix.
+    :param matrix: Matrix with which the matrix which is otherwise created by create_matrix is multiplied
+    :return: The Anti-commutator.
     """
-    temp1 = np.roll(matrix, mat_dim)
-    temp2 = np.roll(matrix, -mat_dim)
-    temp3 = np.roll(matrix.T, mat_dim)
-    temp4 = np.roll(matrix.T, -mat_dim)
-    zeros = np.zeros(mat_dim)
+    temp1 = np.roll(matrix, matrix_dimension)
+    temp2 = np.roll(matrix, -matrix_dimension)
+    temp3 = np.roll(matrix.T, matrix_dimension)
+    temp4 = np.roll(matrix.T, -matrix_dimension)
+    zeros = np.zeros(matrix_dimension)
     temp1[0] = zeros
-    temp2[mat_dim - 1] = zeros
+    temp2[matrix_dimension - 1] = zeros
     temp3[0] = zeros
-    temp4[mat_dim - 1] = zeros
+    temp4[matrix_dimension - 1] = zeros
     return (2. - 4. * number) * matrix + number * (temp1 + temp2 + temp3.T + temp4.T)
 
 
 # print(opt_mat_mul() - mat_mul())
 
 
-def run_mat_mul(matrix1, matrix2, number_of_calls):
+def run_mat_mul(matrix1: np.ndarray, matrix2: np.ndarray, number_of_calls: int):
     """
-
-    :param matrix1:
-    :param matrix2:
-    :param number_of_calls:
-    :return:
+    Runs the mat_mul function with matrix1 and matrix2 number_of_calls times.
+    :param matrix1: A matrix.
+    :param matrix2: Another matrix.
+    :param number_of_calls: How often to call the mat_mul function
+    :return: None.
     """
     for _ in range(number_of_calls):
         mat_mul(matrix1, matrix2)
 
 
-def run_opt_mat_mul(number, matrix, mat_dim, number_of_calls):
+def run_opt_mat_mul(number: float, matrix: np.ndarray, mat_dim: int, number_of_calls: int):
     """
-
-    :param number:
-    :param matrix:
-    :param mat_dim:
-    :param number_of_calls:
-    :return:
+    Runs the opt_mat_mul function number_of_calls times. Matrix is the matrix to multiplied with the dimension mat_dim.
+    :param number: Equivalent of the number used in the create_matrix function.
+    :param matrix: Matrix to be multiplied.
+    :param mat_dim: Dimension of the matrix.
+    :param number_of_calls: How often the opt_mat_mal function is called.
+    :return: None
     """
     for _ in range(number_of_calls):
         opt_mat_mul(number, mat_dim, matrix)
 
 
 @nb.jit(nopython=True)
-def run_jited_mat_mul(matrix1, matrix2, number_of_calls):
+def run_jited_mat_mul(matrix1: np.ndarray, matrix2: np.ndarray, number_of_calls: int) -> np.ndarray:
     """
-
-    :param matrix1:
-    :param matrix2:
-    :param number_of_calls:
-    :return:
+    Jited version of the run_mat_mul function.
     """
     for _ in range(number_of_calls):
         return np.dot(matrix1, matrix2) + np.dot(matrix1, matrix2)
 
 
 @nb.jit(nopython=True)
-def run_jited_opt_mat_mul(matrix, mat_dim, number, number_of_calls):
+def run_jited_opt_mat_mul(matrix: np.ndarray, mat_dim: int, number: float, number_of_calls: int) -> np.ndarray:
     """
-
-    :param number:
-    :param mat_dim:
-    :param matrix:
-    :param number_of_calls:
-    :return:
+    JIted version of the run_opt_mat_mul function.
     """
     for _ in range(number_of_calls):
         temp1 = np.roll(matrix, mat_dim)
@@ -131,9 +106,37 @@ def run_jited_opt_mat_mul(matrix, mat_dim, number, number_of_calls):
         return (2. - 4. * number) * matrix + number * (temp1 + temp2 + temp3.T + temp4.T)
 
 
+# Dimensions used in the dimension benchmark.
+dimensions = np.array([10, 50, 100, 200, 300, 400, 500])
+
+# Fixed dimension for the number of calls benchmark.
+dim = 100
+
+# Number of calls used in the benchmark for the number of calls.
+times_called = np.array([10, 50, 100, 200, 300, 400, 500])
+
+# Fixed number of calls for the dimension benchmark.
+calls = 100
+
+# Arrays saving the data for runs differing how often a function is called but with a fixed dimension.
+normal_time_data = np.array([])
+opt_time_data = np.array([])
+jited_normal_time_data = np.array([])
+jited_opt_time_data = np.array([])
+
+# Arrays saving the data for runs differing in dimension but with a fixing the times a function is called.
+normal_dim_data = np.array([])
+opt_dim_data = np.array([])
+jited_normal_dim_data = np.array([])
+jited_opt_dim_data = np.array([])
+
+# Create a random number and two matrices accordingly.
+num = rd.random()
 multiply_matrix = create_matrix(num, dim)
 state_matrix = np.random.rand(dim, dim)
 
+# Run the following loops for the benchmark.
+# The code could probably be a lot cleaner please don't hate me for this.
 for times in times_called:
     start1 = time.time()
     run_jited_opt_mat_mul(state_matrix, dim, num, times)
@@ -180,28 +183,38 @@ for dimension in dimensions:
     end4 = time.time()
     opt_dim_data = np.append(opt_dim_data, end4 - start4)
 
+# --------------------
+# Plotting the results
+# --------------------
+
+# Axes and fiure
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10., 4.8))
 
+# Graphs for the first plot.
 ax1.plot(times_called, normal_time_data, marker="s")
 ax1.plot(times_called, opt_time_data, marker="o")
 ax1.plot(times_called, jited_normal_time_data, marker="d")
 ax1.plot(times_called, jited_opt_time_data, marker="*")
 
+# Labels ad legends for the first plot
 ax1.set_ylabel("execution time (s)")
-ax1.set_xlabel("times called")
+ax1.set_xlabel("function calls")
 ax1.set_yscale("log")
 ax1.set_title(f"Fixed matrix dimensions: {dim}x{dim}")
 ax1.legend(["Normal", "Optimized", "Jited and normal", "Jited and optimized"], loc="upper left", bbox_to_anchor=(0.075, .995))
 
+# Graphs for the second plot
 ax2.plot(dimensions, normal_dim_data, marker="s")
 ax2.plot(dimensions, opt_dim_data, marker="o")
 ax2.plot(dimensions, jited_normal_dim_data, marker="d")
 ax2.plot(dimensions, jited_opt_dim_data, marker="*")
 
+# Labels and legends for th second plot
 ax2.set_xlabel("matrix dimensions")
 ax2.set_yscale("log")
 ax2.set_title(f"Fixed number of calls: {calls}")
 
+# Show everything
 ax1.grid(True)
 ax2.grid(True)
 plt.tight_layout()
